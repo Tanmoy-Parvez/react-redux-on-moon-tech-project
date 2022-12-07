0258976a1b22f7e112d5aff7d16d1c3c9cfebbbc
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-12-08 00:14:40 +0600

    redux dev tools extensions added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { productReducer } from "./reducers/productReducer";
 
 
 
-export const store = createStore(productReducer)
+export const store = createStore(productReducer, composeWithDevTools())
